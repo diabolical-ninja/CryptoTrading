aa40271a2b94ffc42508b41f1ff55506de67fb2f
Unknown <yeltahir@gmail.com>
2017-08-19 18:03:11 +1000

    Add function to get web description

--- reddit_posts.py
+++ reddit_posts.py
@@ -8,7 +8,8 @@ Date:   2017-08-19
 import praw
 from datetime import datetime
 import yaml
-
+import requests
+from bs4 import BeautifulSoup
 
 
 # Source Credentials
@@ -23,6 +24,23 @@ reddit = praw.Reddit(client_id = creds['reddit']['client_id'],
                      username = creds['reddit']['username'])
 
 
+def get_description(url):
+    """
+    Attempts to retrieve a sites summary/description/blurb
+
+    :param url: The web address
+
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,'lxml')
+    tag = soup.find("meta",  property="og:description")
+    if tag:
+        return tag['content']
+    else:
+        return None
+
+
+
 
 def submission_data(post):
     """
@@ -35,6 +53,7 @@ def submission_data(post):
         'title': post.title,
         'domain': post.domain,
         'url': post.url,
+        'description': get_description(post.url),
         'votes': post.ups - post.downs
     }
 
@@ -47,4 +66,4 @@ def submission_data(post):
 crypto_currency_news = reddit.subreddit('Crypto_Currency_News')
 
 
-ccn_posts = [submission_data(x) for x in crypto_currency_news.top('all')]
+ccn_posts = [submission_data(x) for x in crypto_currency_news.top('all', limit = 10)]
